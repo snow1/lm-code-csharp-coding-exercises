4eafe9108f42deb29ef75e2d595c76539fcb9196
snow <lichunxue96@outlook.com>
2023-09-17 16:01:05 +0200

    1

--- Exercises/Exercise001.cs
+++ Exercises/Exercise001.cs
@@ -9,35 +9,54 @@ namespace Exercises
         public string CapitalizeWord(string word)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            //If the word is null or empty, return the word  
+            if (string.IsNullOrEmpty(word))
+            {
+                return word;
+            }
+            return char.ToUpper(word[0]) + word.Substring(1);
         }
 
         public string GenerateInitials(string firstName, string lastName)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if(string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+                return null;
+            }
+            return firstName[0] + "." + lastName[0];
         }
 
         public double AddVat(double originalPrice, double vatRate)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
-
-            // NB: Look in Exercise001Tests.cs
-            //     There is a test with commented out assertions.
-            //     For an extra challenge, uncomment those assertions and make that test pass too.
+            return Math.Round(originalPrice * (100 + vatRate) / 100, 2);
         }
 
         public string Reverse(string sentence)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+           if(string.IsNullOrEmpty(sentence))
+            {
+                return null;
+            }
+            char[] charArray = sentence.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
 
         public int CountLinuxUsers(List<User> users)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            int count = 0;
+            if(users == null)
+            {
+                return count;
+            }
+            foreach (User user in users)
+            {
+                if (user.Type == "Linux")
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
